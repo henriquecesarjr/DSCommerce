1cc80be9f43a41cd4493718c1f7cee0ebb2ebf20
henriquecesarjrcm <contato@henriquecesarjrcm.com>
2023-10-06 15:07:17 -0300

    Product e Category

---
+++ src/main/java/com/devsuperior/dscommerce/entities/Product.java
@@ -0,0 +1,81 @@
+package com.devsuperior.dscommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    private Double price;
+    private String imgUri;
+
+    @ManyToMany
+    @JoinTable(name = "tb_product_category",
+            joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories = new HashSet<>();
+
+    public Product(){
+    }
+
+    public Product(Long id, String nome, String description, Double price, String imgUri) {
+        this.id = id;
+        this.nome = nome;
+        this.description = description;
+        this.price = price;
+        this.imgUri = imgUri;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getImgUri() {
+        return imgUri;
+    }
+
+    public void setImgUri(String imgUri) {
+        this.imgUri = imgUri;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+}
