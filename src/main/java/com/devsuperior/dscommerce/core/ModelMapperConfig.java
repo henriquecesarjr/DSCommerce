07b1e23f3c1ede6e48ef6ed4ea524454a02849b3
henriquecesarjrcm <contato@henriquecesarjrcm.com>
2023-10-12 19:20:08 -0300

    modelmapper

---
+++ src/main/java/com/devsuperior/dscommerce/core/ModelMapperConfig.java
@@ -0,0 +1,15 @@
+package com.devsuperior.dscommerce.core;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.Banner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -3,11 +3,10 @@ package com.devsuperior.dscommerce.services;
 import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -15,9 +14,12 @@ public class ProductService {
     @Autowired
     private ProductRepository repository;
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     @Transactional(readOnly = true)
     public ProductDTO findById(Long id) {
         Product product = repository.findById(id).get();
-        return new ProductDTO(product);
+        return modelMapper.map(product, ProductDTO.class);
     }
 }
