9dd311be884c4ac0f8ebc36dd2259343a4d296c6
henriquecesarjrcm <contato@henriquecesarjrcm.com>
2023-10-12 22:10:31 -0300

    Correcao modelmapper

--- src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -28,6 +28,26 @@ public class ProductDTO {
         imgUrl = entity.getImgUrl();
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
     public Long getId() {
         return id;
     }
